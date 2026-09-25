2a018a4c7cb54066225b0be9a23111e803da1228
AartiPatil <aratipp2003@example.com>
2026-09-25 17:24:34 +0530

    Fix access token local storage key

--- client/src/utils/Axios.js
+++ client/src/utils/Axios.js
@@ -9,7 +9,7 @@ const Axios = axios.create({
 //sending access token in the header
 Axios.interceptors.request.use(
     async(config)=>{
-        const accessToken = localStorage.getItem('accesstoken')
+        const accessToken = localStorage.getItem('accessToken')
 
         if(accessToken){
             config.headers.Authorization = `Bearer ${accessToken}`
