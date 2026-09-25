b2026c643819f582c22446db47b99fb24155cbee
AartiPatil <aratipp2003@example.com>
2026-09-25 23:31:38 +0530

    Fix SummaryApi syntax error

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -25,7 +25,7 @@ const SummaryApi = {
     refreshToken : { 
     url : '/api/user/refresh-token', 
     method : 'post' 
-}
+},
     userDetails : {
         url : '/api/user/user-details',
         method : "get"
